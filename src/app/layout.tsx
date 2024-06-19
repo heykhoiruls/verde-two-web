d8964d7e28c34c55ba986908ef13675410611a02
khoiruuuls <2010631170092@student.unsika.ac.id>
2024-06-19 08:51:57 +0700

    another commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar copy 2";
+import Navbar from "./components/navbar/Navbar";
 import CallActionButton from "./components/button/CallActionButton";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Verde Two",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type RootLayoutProps = {
   children: React.ReactNode;
-}>) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className="h-dvh">
         <Navbar />
         <CallActionButton />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
